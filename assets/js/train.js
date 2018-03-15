63f6d76f7ece99cfacf090d7f33b3307f6a55b2b
snavdeepsingh <snavdeepsingh@gmail.com>
2018-03-15 16:30:00 -0500

    add firebase

---
+++ assets/js/train.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyBrRvJZIhjETNUm9b3tiyCpI_7J5nqxCes",
+    authDomain: "train-scheduler-5dc0d.firebaseapp.com",
+    databaseURL: "https://train-scheduler-5dc0d.firebaseio.com",
+    projectId: "train-scheduler-5dc0d",
+    storageBucket: "train-scheduler-5dc0d.appspot.com",
+    messagingSenderId: "302637401147"
+  };
+ firebase.initializeApp(config);
